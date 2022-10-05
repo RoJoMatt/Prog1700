d3fd5b10145047b4321367307291ae998329fa88
RoJoMatt <rojomatt85@gmail.com>
2022-10-05 16:09:10 -0300

    Final sync for the day

---
+++ Story Time 1/test.py
@@ -0,0 +1,13 @@
+"""
+Exercise: Story Time Part I
+
+Program: Exercise: Story Time Part I.py
+Author: Joe Mattinson
+Last Date Modified: 21/09/22
+The purpose of this program is to demonstrate the basic usages
+of variables and comments.
+
+This comment is a docstring 
+
+"""
+
